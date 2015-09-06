84e809521e6d42629a14f15cb066fc659bbaa697
mronian <anchitn@gmail.com>
2015-09-06 18:32:29 +0530

    Data Collection

--- orig_live_plotter.py
+++ orig_live_plotter.py
@@ -3,14 +3,12 @@ import socket
 import sys
 import time
 import numpy as np
-import sklearn as skml
 import matplotlib.pyplot as plt
 import random
 import filter
-import math
 #let's set up some constants
 #HOST="78.91.80.123"
-PORT = 8080   #arbitrary port not currently in use
+PORT = 8000   #arbitrary port not currently in use
 #ADDR = (HOST,PORT)    #we need a tuple for the address
 BUFSIZE = 4096    #reasonably sized buffer for data
 serv=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -24,78 +22,38 @@ def listen():
     try:
         serv.bind((HOST, PORT))
         serv.listen(5)
+        print "server listening"
+        conn,addr = serv.accept()
+        print "Koi toh mila!"
     except KeyboardInterrupt:
         print "Keyboard Interrupt"
         serv.close()
         exit(1)
-    f=open('Exercise1.dat', 'w')
-    lin=np.zeros(6)
-    limit=0
-    check=0
-    j=0
-    while j<20 :
-        i=0
-        print j
-        try:
-            print "server listening"
-            conn,addr = serv.accept()
-            print "Koi toh mila!" + str(j)
-            while True:
-                data=conn.recv(4096)
-                sys.stdout.flush()
-                readings = data.split('\n')[0]
-                readings=readings.split(',')
-                if readings[0]=="start":
-                    check=1
-                    i=0
-                    j=j+1
-                    continue
-                if readings[0]=="Stop":
-                    check=0
-                    print " kjgkjjkgkgjkg"
-                    break
-                if check==1:
-                    i=i+1
-                    if readings[0]=="lin":
-                        lin[0]=float(readings[1])
-                        lin[1]=float(readings[2])
-                        lin[2]=float(readings[3])
-                    if readings[0]=="ori":
-                        lin[3]=float(readings[1])
-                        lin[4]=float(readings[2])
-                        lin[5]=float(readings[3])
-                        f.write(str(lin[0])+" "+str(lin[1])+" "+str(lin[2])+" "+str(lin[3])+" "+str(lin[4])+" "+str(lin[5])+" "+str(i)+" "+str(j)+"\n")
+    try:
+        while True:
+            data=conn.recv(4096)
+            sys.stdout.flush()
+            print data
+            readings = data.split(',')
+            if len(readings) == 4:
+                new_plot(readings[0].strip(),float(readings[1].strip()), float(readings[2].strip()), float(readings[3].strip()))
+        conn.close()
+        sleep(10)
 
-            conn.close()
-        except KeyboardInterrupt:
-            conn.close()
-            print "bye!"
-        except IndexError:
-            conn.close()
-            print "indexError"
-        # if len(readings) == 4:
-        #     new_plot(readings[0].strip(),float(readings[1].strip()), float(readings[2].strip()), float(readings[3].strip()))
-        # print limit
-        # for i in xrange(3):
-        #     plt.plot(lin[0:limit,i])
-        #     plt.ylabel("Lin"+str(i))
-        #     plt.savefig("Lin"+str(i)+".png")
-        # for i in xrange(3):
-        #     plt.plot(ori[0:limit,i])
-        #     plt.ylabel("Ori"+str(i))
-        #     plt.savefig("Ori"+str(i)+".png")
-
+    except KeyboardInterrupt:
+        conn.close()
+        print "bye!"
+    except IndexError:
+        conn.close()
+        print "indexError"
 
 """ globals, parameter_dict -> stores the list for corresponding sensor """
 # data for each sensor
 sensors_data = dict()
-#directions = ['x', 'y', 'z']
-directions = ['x']
+directions = ['x', 'y', 'z']
 period = 500
 time1 = range(0,period)
 # the following function is used to plot data
-def magnitude(x, y, z):
-    return math.sqrt(x**2 + y**2 + z**2)
 def new_plot(sensor, x, y, z):
     cur_time = int(round(time.time() * 1000))
     if sensor not in sensors_data:
@@ -109,47 +67,46 @@ def new_plot(sensor, x, y, z):
             sensors_data[sensor]['time'] = list()
             sensors_data[sensor][direction]['temp_data'] = list()
             sensors_data[sensor][direction]['k_filtering_temp_data'] = list()
-            li, = sensors_data[sensor][direction]['axis'].plot(time1, sensors_data[sensor][direction]['data'], 'r')
+            li, = sensors_data[sensor][direction]['axis'].plot(time1, sensors_data[sensor]['x']['data'], 'r')
             sensors_data[sensor][direction]['list'] = li
             li, = sensors_data[sensor][direction]['axis'].plot(time1, sensors_data[sensor][direction]['k_filtering_data'], 'b')
             sensors_data[sensor][direction]['k_filtering_list'] = li
             sensors_data[sensor][direction]['figure'].canvas.set_window_title(sensor+"_"+direction)
             sensors_data[sensor][direction]['figure'].canvas.draw()
             plt.show(block=False)
-            plt.ylim(-5,5)
+            plt.ylim(-10,10)
     sensors_data[sensor]['time'].append(cur_time)
-    #sensors_data[sensor]['x']['temp_data'].append(magnitude(x,y,z))
-    if sensor == 'ori':
-        sensors_data[sensor]['x']['temp_data'].append(y/36.0)
-    elif sensor == 'lin':
-        sensors_data[sensor]['x']['temp_data'].append(z/2)
-
+    sensors_data[sensor]['x']['temp_data'].append(x)
+    sensors_data[sensor]['y']['temp_data'].append(y)
+    sensors_data[sensor]['z']['temp_data'].append(z)
+    print len(sensors_data[sensor]['x']['data']), len(sensors_data[sensor]['x']['k_filtering_data'])
     if len(sensors_data[sensor]['x']['k_filtering_temp_data'])==0:
         for direction in directions:
             sensors_data[sensor][direction]['k_filtering_temp_data'].append(sensors_data[sensor][direction]['temp_data'][-1])
     else:
         for direction in directions:
             sensors_data[sensor][direction]['k_filtering_temp_data'].append(filter.k_filtering(sensors_data[sensor][direction]['k_filtering_temp_data'][-1], sensors_data[sensor][direction]['temp_data'][-1]))
-
     if len(sensors_data[sensor]['x']['temp_data']) == 10:
         for direction in directions:
             sensors_data[sensor][direction]['data'][:-10] = sensors_data[sensor][direction]['data'][10:]
             sensors_data[sensor][direction]['data'][-10:] = sensors_data[sensor][direction]['temp_data']
             sensors_data[sensor][direction]['list'].set_ydata(sensors_data[sensor][direction]['data'])
-            
             sensors_data[sensor][direction]['k_filtering_data'][:-10] = sensors_data[sensor][direction]['k_filtering_data'][10:]
             sensors_data[sensor][direction]['k_filtering_data'][-10:] = sensors_data[sensor][direction]['k_filtering_temp_data']
             sensors_data[sensor][direction]['k_filtering_list'].set_ydata(sensors_data[sensor][direction]['k_filtering_data'])
-#            print len(sensors_data[sensor][direction]['k_filtering_temp_data']), len(sensors_data[sensor][direction]['k_filtering_data']), len(sensors_data[sensor][direction]['data']), len(time1)
-            print "redrawn"
+
+            #print len(sensors_data[sensor][direction]['k_filtering_temp_data']), len(sensors_data[sensor][direction]['k_filtering_data']), len(sensors_data[sensor][direction]['data']), len(time1)
+            cur_time = int(round(time.time() * 1000))
+            print "Redrawing " + str(cur_time)
             sensors_data[sensor][direction]['figure'].canvas.draw()
             sensors_data[sensor][direction]['temp_data'] = list()
-            sensors_data[sensor][direction]['k_filtering_temp_data'] = list() 
+            sensors_data[sensor][direction]['k_filtering_temp_data'] = list()
 
 
 
-listen()
-"""
+#listen()
+
+# this code was used for offline testing
 
 for i in range(1,1000):
     try:
@@ -157,5 +114,4 @@ for i in range(1,1000):
         time.sleep(0.01)
     except KeyboardInterrupt:
         break
-
-"""
+	
